7cae5450c618c0e3b92115b7eadad0cea55128e4
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-05-06 15:11:40 -0300

    commit

---
+++ cap06/js/ex6_1.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form")
+const respNome = document.querySelector("span")
+const respLista = document.querySelector("pre")
+
+const paciente = []
+
+form.addEventListener("submit", (e) =>{
+    
+})
