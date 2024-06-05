a8e333fade3d8f4854f858a920747043356c33a8
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-06-05 11:02:46 -0300

    commit

---
+++ cap07/js/ex7_b.js
@@ -0,0 +1,11 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    const mensagem = frm.palindromo.value
+    var tam = mensagem.length
+
+
+}) 
