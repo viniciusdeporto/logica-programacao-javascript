600782733e257658bfdf35446bf00d169d6d3967
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-06-06 08:41:06 -0300

    commit

--- cap07/js/ex7_b.js
+++ cap07/js/ex7_b.js
@@ -1,11 +1,29 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const mensagem = frm.palindromo.value
     var tam = mensagem.length
+    let invertida = ""
+    let resultado = ""
+
+    for (let i = tam - 1; i >= 0; i--) {
+        invertida += mensagem.charAt(i)
+
+    }
 
 
+
+    if (invertida === mensagem) {
+
+        resp.innerText = `Frase Invertida: ${invertida}\n${mensagem}, é um palindromo`
+
+    } else {
+        resp.innerText = `${mensagem} não é um Palindromo`
+    }
+
+
+    
 }) 
